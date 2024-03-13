7b3bf5fa442342687c7efe3974ab0bb96d3e3634
Levítico Rimon <leviticorimon@gmail.com>
2024-03-12 21:32:55 -0300

    Adicionada estrutura inicial do arquivo main.c

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void exibir_menu(void) {
+    printf(
+        "======= MENU =======\n"
+        "1. Adicionar recinto\n"
+        "2. Remover recinto\n"
+        "3. Adicionar animal\n"
+        "4. Remover animal\n"
+        "5. Editar informacao de animal\n"
+        "6. Buscar animal por nome\n"
+        "7. Listar recintos e os animais presentes\n"
+        "8. Sair\n"
+        "====================\n"
+    );
+}
+
+int main() {
+    int opcao = 0;
+
+    while(opcao != 8){
+        exibir_menu();
+        printf("Escolha uma opcao do menu: ");
+        scanf("%d", &opcao);
+
+        switch(opcao) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                printf("Programa encerrado.\n");
+                break;
+            default:
+                printf("* Opcao invalida! *\n");
+                break;
+        }
+    }
+    return 0;
+}
