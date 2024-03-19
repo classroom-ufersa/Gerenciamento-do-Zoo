633177d8d0ff03ffd5ef61fb61832e370d669349
EvelynAires <cris>
2024-03-19 14:03:23 -0300

    .

---
+++ TAD-recinto/recinto.c
@@ -0,0 +1,11 @@
+#include "animal.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+struct recinto{
+    int identificacao;
+    int capacidade;
+    char horario[50];
+    char animaispre[50];
+};
