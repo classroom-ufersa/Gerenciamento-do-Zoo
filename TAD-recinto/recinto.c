f5891c4ca48bf7c89070216727deffc316d561e0
EvelynAires <cris>
2024-03-23 16:56:30 -0300

    Criação da função de insere_recinto_lista

--- TAD-recinto/recinto.c
+++ TAD-recinto/recinto.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 struct recinto{
-    int identificacao;
+    int id;
     int capacidade;
     char horario[50];
     char animaispre[50];
@@ -15,4 +15,28 @@ Recinto * cria_lista_recinto(void) {
     return NULL;
 }
 
+Recinto * insere_recinto_lista(Recinto * lista, int id, int capacidade, char horario[], char animaispre[]) {
+    Recinto * novo_elemento = (Recinto *) malloc(sizeof(Recinto));
+    if(novo_elemento == NULL) {
+        printf("Falha na alocacao de memoria.\n");
+        exit(1);
+    }
+    novo_elemento->id = id;
+    novo_elemento->capacidade = capacidade;
+    strcpy(novo_elemento->horario, horario);
+    strcpy(novo_elemento->animaispre, animaispre);
 
+    if(lista == NULL || id < lista->id) {
+        novo_elemento->pro_elemento = lista;
+        return novo_elemento;
+    }
+
+    Recinto * elemento_atual = lista;
+    while(elemento_atual->pro_elemento != NULL && id > elemento_atual->pro_elemento->id) {
+        elemento_atual = elemento_atual->pro_elemento;
+    }
+    novo_elemento->pro_elemento = elemento_atual->pro_elemento;
+    elemento_atual->pro_elemento = novo_elemento;
+
+    return lista;
+}
--- TAD-recinto/recinto.h
+++ TAD-recinto/recinto.h
@@ -3,3 +3,6 @@ typedef struct recinto Recinto;
 
 /* Função que inicializa uma lista de recintos retornando NULL */
 Recinto * cria_lista_recinto(void);
+
+/*Função que insere um novo recinto na lista encadeada ordenado por id*/
+Recinto * insere_recinto_lista(Recinto * lista, int id, int capacidade, char horario[], char animaispre[]);
